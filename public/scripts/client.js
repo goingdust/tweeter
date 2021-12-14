9755a11024652d44ff02af2fc16c558566a79beb
goingdust <dustinluck@gmail.com>
2021-12-14 19:35:40 +0000

    Write up the tweet section articles of index.html with semantic tags like header and footer. Style the article with flexboxes and fontawesome icons. Write jQuery event handlers in client.js that applies/takes away a box-shadow to article on mouseover/mouseleave and does the same with a color change to the icons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const $icons = $('i.fas');
+  const $tweets = $('article.tweet');
+  
+  $tweets.on('mouseover', function() {
+    this.style.boxShadow = '5px 5px 10px #7563317c';
+  });
+
+  $tweets.on('mouseleave', function() {
+    this.style.boxShadow = 'none';
+  });
+  
+  $icons.on('mouseover', function() {
+    this.style.color = 'rgb(255, 60, 0)';
+  });
+
+  $icons.on('mouseleave', function() {
+    this.style.color = '#35cf7098';
+  });
+})
