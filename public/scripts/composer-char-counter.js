1cb39a0e6baf6aef39c88a07c7b70d6a40f3ee4b
goingdust <dustinluck@gmail.com>
2021-12-14 04:48:59 +0000

    Write countChars function that prints out the num of chars in the textarea input field and turns negative/red when over 140 chars. Implement it into keypress and keydown event handlers

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  console.log('hey der');
+  
+  $('textarea').on('keypress', function(event) {
+    const characters = $(this).val().length + 1;
+    const counter = $(this.closest('form')).find('output.counter')[0];
+    const key = event.type;
+
+    countChars(characters, counter, key);
+  });
+
+  $('textarea').on('keydown', function(event) {
+    const characters = $(this).val().length - 1;
+    const counter = $(this.closest('form')).find('output.counter')[0];
+    const key = event.key;
+
+    if (characters > -1 && key === 'Backspace' || key === 'Delete') {
+      countChars(characters, counter, key);
+    }
+  });
+});
+
+const countChars = (characters, counter, key) => {
+  if (characters > 140 && characters <= 141) {
+    counter.innerHTML = -1;
+    counter.style.color = 'red';
+  } else if (characters > 141) {
+    if (key === 'keypress') {
+      counter.innerHTML -= 1;
+    } else {
+      counter.innerHTML -= -1;
+    }
+  } else {
+    counter.style.color = '#545149';
+    counter.innerHTML = characters;
+  }
+};
