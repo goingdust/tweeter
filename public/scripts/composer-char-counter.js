88183c69722c2b7cbfb0e10371624b63106e7ea2
goingdust <dustinluck@gmail.com>
2021-12-16 00:01:22 +0000

    Add .hide at the beginning of composer-form-toggle to initially hide new-tweet section and then .toggle it when the button in the nav bar is clicked. Write css animation so that pointer and arrow icon change when hovered over

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  
+  const countChars = (characters, counter, key) => {
+    if (characters > 140 && characters <= 141) {
+      counter.innerHTML = -1;
+      $(counter).addClass('counter-red');
+    } else if (characters > 141) {
+      if (key === 'keypress') {
+        counter.innerHTML -= 1;
+      } else {
+        counter.innerHTML -= -1;
+      }
+    } else if (characters >= 0) {
+      $(counter).removeClass('counter-red');
+      counter.innerHTML = characters;
+    } else if (characters < 0) {
+      $(counter).removeClass('counter-red');
+      counter.innerHTML = 0;
+    }
+  };
+  
   $('textarea').on('keypress', function(event) {
     const characters = $(this).val().length + 1;
     const counter = $(this.closest('form')).find('output.counter')[0];
@@ -17,22 +37,3 @@ $(document).ready(function() {
     }
   });
 });
-
-const countChars = (characters, counter, key) => {
-  if (characters > 140 && characters <= 141) {
-    counter.innerHTML = -1;
-    $(counter).addClass('counter-red');
-  } else if (characters > 141) {
-    if (key === 'keypress') {
-      counter.innerHTML -= 1;
-    } else {
-      counter.innerHTML -= -1;
-    }
-  } else if (characters >= 0) {
-    $(counter).removeClass('counter-red');
-    counter.innerHTML = characters;
-  } else if (characters < 0) {
-    $(counter).removeClass('counter-red');
-    counter.innerHTML = 0;
-  }
-};
--- public/scripts/composer-form-toggle.js
+++ public/scripts/composer-form-toggle.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-
-
   
+  const $newTweetSection = $($(document).find('body')).find('section.new-tweet');
+  $newTweetSection.hide();
+  
+  const $formToggleButton = $($($(document).find('body')).find('nav')).find('div');
+
+  $formToggleButton.on('click', function(event) {
+
+    $newTweetSection.toggle();
+  });
+
 });
